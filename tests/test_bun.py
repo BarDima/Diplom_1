648b3b3e3b1bdd81a66e5f5e9e10db1e7028583e
Dima Barovskiy <barovskiy@me.com>
2024-07-04 16:00:44 +0300

    correction_test_bun

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,13 +1,19 @@
 import pytest
 from praktikum.bun import Bun
 
-
 class TestBun:
     @pytest.mark.parametrize("name, price", [
         ('Флюоресцентная булка R2-D3', 988),
         ('Краторная булка N-200i', 1255),
     ])
-    def test_bun_parameters(self, name, price):
+    def test_bun_name(self, name, price):
         bun = Bun(name, price)
         assert bun.get_name() == name
+
+    @pytest.mark.parametrize("name, price", [
+        ('Флюоресцентная булка R2-D3', 988),
+        ('Краторная булка N-200i', 1255),
+    ])
+    def test_bun_price(self, name, price):
+        bun = Bun(name, price)
         assert bun.get_price() == price
